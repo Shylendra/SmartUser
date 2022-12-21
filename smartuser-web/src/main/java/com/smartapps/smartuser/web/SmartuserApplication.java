824fecb66fb1fb877a951c59da5d10ad422cc4c4
shylendra <shylendra.tm@gmail.com>
2022-12-21 21:55:13 +1100

    Updated micro-services Eureka config

--- smartuser-web/src/main/java/com/smartapps/smartuser/web/SmartuserApplication.java
+++ smartuser-web/src/main/java/com/smartapps/smartuser/web/SmartuserApplication.java
@@ -3,10 +3,11 @@ package com.smartapps.smartuser.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableCaching
 @SpringBootApplication
+@EnableEurekaClient
 public class SmartuserApplication {
 
 	public static void main(String[] args) {
